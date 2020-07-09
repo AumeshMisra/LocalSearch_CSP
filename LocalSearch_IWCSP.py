ea6d7106be258e527bcec9215a85f147c44f8758
Aumesh Misra <aumeshmisra@wustl.edu>
2020-07-09 12:53:45 -0700

    updated to include elicitation when getting starting assing.

--- LocalSearch_IWCSP.py
+++ LocalSearch_IWCSP.py
@@ -24,6 +24,25 @@ class LocalSearch_IWCSP(LocalSearchProblem):
     #choose random starting assignment
     def get_starting_assign(self):
         starting_dict = {key: random.randint(0, self.domainrange-1) for key in self.varList}
+        scopes = self.incompTable.keys()
+        answer_list = list(starting_dict)
+        comb = [(str(x) + ' ' +  str(y)) for idx, x in enumerate(answer_list) for y in answer_list[idx + 1: ]]
+        print (starting_dict)
+        print (self.incompTable)
+        #iterate through constraints and update the incomplete table with elicited values
+        for scope in comb:
+            if scope in self.incompTable or scope[::-1] in self.incompTable:
+                if scope[::-1] in self.incompTable:
+                    scope = scope[::-1]
+                scope_values = [int(i) for i in scope.split()]
+                row_cell = starting_dict[scope_values[0]]
+                column_cell = starting_dict[scope_values[1]]
+
+                if self.incompTable[scope][row_cell][column_cell] == '?':
+                    elicited_value = self.oracleTable[scope][row_cell][column_cell]
+                    self.incompTable[scope][row_cell][column_cell] = elicited_value
+
+        print (self.incompTable)
         return starting_dict
 
 
@@ -52,7 +71,7 @@ class LocalSearch_IWCSP(LocalSearchProblem):
         best_variable = 0
         max_val = float('-inf')
 
-        print (local_pref_dict)
+        #print (local_pref_dict)
         for key in local_pref_dict:
             if sum(local_pref_dict[key]) > max_val:
                 best_variable = key
@@ -72,9 +91,9 @@ class LocalSearch_IWCSP(LocalSearchProblem):
                 preferences[value] = self.compute_preference(new_assign)
 
         #we want the value that minimizes the cost
-        print(preferences)
+        #print(preferences)
         best_value = min(preferences, key=preferences.get)
-        print (best_value)
+        #print (best_value)
         return best_value
 
     #computes the preference of a given assignment
@@ -104,10 +123,10 @@ class LocalSearch_IWCSP(LocalSearchProblem):
 
 
 LSP = LocalSearch_IWCSP('1')
-print(LSP.current_assign)
+#print(LSP.current_assign)
 var = LSP.choose_variable()
-print('var: ' + str(var))
+#print('var: ' + str(var))
 value = LSP.choose_value_for_variable(var)
 LSP.update_assign(var, value)
-print (LSP.current_assign)
+#print (LSP.current_assign)
 LSP.choose_variable()
