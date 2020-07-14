f03e90920a8645216986db869c30da2a88c82c1c
Aumesh Misra <aumeshmisra@wustl.edu>
2020-07-14 14:13:52 -0700

    started getting experimental results

--- LocalSearch_IWCSP.py
+++ LocalSearch_IWCSP.py
@@ -1,4 +1,5 @@
 import random
+import time
 from itertools import combinations
 
 import readInput
@@ -9,7 +10,7 @@ class LocalSearch_IWCSP(LocalSearchProblem):
     #name: filename
     def __init__(self, name, tabu_list_maxsize):
         path = './'
-        xmlfile = 'input_files/Rnd3-2.xml'
+        xmlfile = 'input_files/Rnd10-3-1.xml'
         incomp = open(path + 'output-Incomp'+'-'+name+'.txt', 'r')
         oracle = open(path + 'oracle'+'-'+name+'.txt', 'r')
         elicit = open(path + 'elicit'+'-'+name+'.txt', 'r')
@@ -19,13 +20,16 @@ class LocalSearch_IWCSP(LocalSearchProblem):
         self.incompTable, variables, varDomain = self.input.readIncomp(incomp)
         self.oracleTable = self.input.readOracle(oracle, self.domainrange)
         self.elicitationTable = self.input.readElicitationCost(elicit, self.domainrange)
-        self.current_assign = self.get_starting_assign()
+        self.elicitation_number = 0
+        self.elicitation_cost = 0
         self.tabu_list = []
         self.tabu_list_maxsize = tabu_list_maxsize
+        self.current_assign = self.get_starting_assign()
+
 
     #choose random starting assignment
     def get_starting_assign(self):
-        starting_dict = {key: random.randint(0, self.domainrange-1) for key in self.varList}
+        starting_dict = {key: random.randint(1, self.domainrange) for key in self.varList}
         scopes = self.incompTable.keys()
         answer_list = list(starting_dict)
         comb = [(str(x) + ' ' +  str(y)) for idx, x in enumerate(answer_list) for y in answer_list[idx + 1: ]]
@@ -35,12 +39,15 @@ class LocalSearch_IWCSP(LocalSearchProblem):
                 if scope[::-1] in self.incompTable:
                     scope = scope[::-1]
                 scope_values = [int(i) for i in scope.split()]
-                row_cell = starting_dict[scope_values[0]]
-                column_cell = starting_dict[scope_values[1]]
+                row_cell = starting_dict[scope_values[0]] - 1
+                column_cell = starting_dict[scope_values[1]] - 1
 
                 if self.incompTable[scope][row_cell][column_cell] == '?':
                     elicited_value = self.oracleTable[scope][row_cell][column_cell]
                     self.incompTable[scope][row_cell][column_cell] = elicited_value
+                    elicitation_cost = self.elicitationTable[scope][row_cell][column_cell]
+                    self.elicitation_cost += int(elicitation_cost)
+                    self.elicitation_number += 1
 
         return starting_dict
 
@@ -56,8 +63,8 @@ class LocalSearch_IWCSP(LocalSearchProblem):
                 if scope[::-1] in self.incompTable:
                     scope = scope[::-1]
                 scope_values = [int(i) for i in scope.split()]
-                row_cell = self.current_assign[scope_values[0]]
-                column_cell = self.current_assign[scope_values[1]]
+                row_cell = self.current_assign[scope_values[0]] - 1
+                column_cell = self.current_assign[scope_values[1]] - 1
 
                 if self.incompTable[scope][row_cell][column_cell] == '?':
                     constraint_value = 0
@@ -91,14 +98,25 @@ class LocalSearch_IWCSP(LocalSearchProblem):
         old_value = self.current_assign[variable]
         preferences = {}
 
-        for value in range(0,self.domainrange):
+        for value in range(1,self.domainrange+1):
             if value != old_value:
                 new_assign = self.current_assign.copy()
                 new_assign[variable] = value
                 preferences[value] = self.compute_preference(new_assign)
 
         #we want the value that minimizes the cost
-        best_value = min(preferences, key=preferences.get)
+        best_value = None
+        min = float('inf')
+
+        for value in preferences:
+            if preferences[value][0] < min:
+                best_value = value
+                min = preferences[value][0]
+            elif (preferences[value][0] == min):
+                if (preferences[value][1] < preferences[best_value][1]):
+                    best_value = value
+                    min = preferences[value][0]
+
         return best_value
 
     #computes the preference of a given assignment
@@ -107,27 +125,33 @@ class LocalSearch_IWCSP(LocalSearchProblem):
         answer_list = list(assignment)
         comb = [(str(x) + ' ' +  str(y)) for idx, x in enumerate(answer_list) for y in answer_list[idx + 1: ]]
         preference_val = 0
+        count = 0
         for scope in comb:
             if scope in self.incompTable or scope[::-1] in self.incompTable:
                 if scope[::-1] in self.incompTable:
                     scope = scope[::-1]
                 scope_values = [int(i) for i in scope.split()]
 
-                row_cell = assignment[scope_values[0]]
-                column_cell = assignment[scope_values[1]]
+                row_cell = assignment[scope_values[0]] - 1
+                column_cell = assignment[scope_values[1]] - 1
 
                 if (elicit):
                     if self.incompTable[scope][row_cell][column_cell] == '?':
                         elicited_value = self.oracleTable[scope][row_cell][column_cell]
                         self.incompTable[scope][row_cell][column_cell] = elicited_value
+                        elicitation_cost = self.elicitationTable[scope][row_cell][column_cell]
+                        self.elicitation_cost += int(elicitation_cost)
+                        self.elicitation_number += 1
                     constraint_value = int(self.incompTable[scope][row_cell][column_cell])
                     preference_val += constraint_value
                 else:
                     if self.incompTable[scope][row_cell][column_cell] != '?':
                         constraint_value = int(self.incompTable[scope][row_cell][column_cell])
                         preference_val += constraint_value
+                    else:
+                        count += 1
 
-        return preference_val
+        return ([preference_val, count])
 
     #updates assignment
     def update_assign(self, variable, value):
@@ -135,19 +159,23 @@ class LocalSearch_IWCSP(LocalSearchProblem):
         new_assign[variable] = value
 
         if (variable is None):
+            self.tabu_list.append(variable)
+            if (len(self.tabu_list) > self.tabu_list_maxsize):
+                self.tabu_list.pop(0)
             return
 
-        if self.compute_preference(new_assign, elicit = True) < self.compute_preference(self.current_assign):
-            print ("changed variable!")
+        #compares two lists using lexigraphical ordering
+        if self.compute_preference(new_assign, elicit = True) < self.compute_preference(self.current_assign, elicit = True):
+            #print ("changed variable!")
             self.current_assign[variable] = value
         else:
             if variable not in self.tabu_list:
-                print ("added tabu")
+                #print ("added tabu")
                 self.tabu_list.append(variable)
             if (len(self.tabu_list) > self.tabu_list_maxsize):
                 self.tabu_list.pop(0)
 
-    def solve(self, iterations = 100, p = 0.00, t = 0):
+    def solve(self, iterations = 0, p = 0.00):
         for i in range(0, iterations):
             random_step_chance = random.random()
             if (random_step_chance > p):
@@ -159,15 +187,32 @@ class LocalSearch_IWCSP(LocalSearchProblem):
 
         return self.current_assign
 
-LSP = LocalSearch_IWCSP('1', 3)
-#print(LSP.current_assign)
-# var = LSP.choose_variable()
-# #print('var: ' + str(var))
-# value = LSP.choose_value_for_variable(var)
-# LSP.update_assign(var, value)
-# #print (LSP.current_assign)
-# LSP.choose_variable()
-#print (LSP.varList)
-LSP.solve(iterations = 1000, p = 0.20)
-print (LSP.current_assign)
-print (LSP.compute_preference(LSP.current_assign))
+preferences = []
+runtimes = []
+elicitation_cost = []
+elicitation_numbers = []
+
+
+for i in range(0,10):
+
+    start = time.time()
+    LSP = LocalSearch_IWCSP('1', 1000)
+    LSP.solve(iterations = 100000, p = 0.20)
+
+    end = time.time()
+    runtime = end - start
+    preferences.append(LSP.compute_preference(LSP.current_assign)[0])
+    runtimes.append(runtime)
+    elicitation_cost.append(LSP.elicitation_cost)
+    print (LSP.elicitation_cost)
+    elicitation_numbers.append(LSP.elicitation_number)
+
+    # print (LSP.current_assign)
+    # print (LSP.compute_preference(LSP.current_assign))
+    # print (runtime)
+    # print (LSP.elicitation_number)
+
+print (sum(preferences)/ len(preferences))
+print (sum(runtimes)/ len(runtimes))
+print (sum(elicitation_cost)/ len(elicitation_cost))
+print (sum(elicitation_numbers)/ len(elicitation_numbers))
--- readme.txt
+++ readme.txt
@@ -2,23 +2,22 @@ How to run the program for IWCSPs+EC
 
 #Step1: We use a randomly generated problem in xml format and use the following code (readInput.py) to convert it to an incomplete problem with some unknown constraint costs as follows:
 
- Input arguments in order of appearance: {xml file}, {1: is the command to assign a number of unknown costs to the whole problem or 2: every constraint will get some percentage of unknown costs}, {number of unknowns in total or percentage of unknown costs in every constraints}, {path to generate the output}, {name of the output file} 
+ Input arguments in order of appearance: {xml file}, {1: is the command to assign a number of unknown costs to the whole problem or 2: every constraint will get some percentage of unknown costs}, {number of unknowns in total or percentage of unknown costs in every constraints}, {path to generate the output}, {name of the output file}
 
 Example: python readInput.py Rnd5-7-1.xml 2 40 ./ 1
 
-#Step2: Given the generated problem outputs with unknown constraint costs, we use the following code (GenerateTree.py) to solve the problem 
-Input arguments in order of appearance: {weight}, {granularity}, {heuristic method}, {type of weight}, {with or without elicitation cost}, {path to read the output problem}, {name of the output file}   
+#Step2: Given the generated problem outputs with unknown constraint costs, we use the following code (GenerateTree.py) to solve the problem
+Input arguments in order of appearance: {weight}, {granularity}, {heuristic method}, {type of weight}, {with or without elicitation cost}, {path to read the output problem}, {name of the output file}
 
 {weight}: We use an additive weight or a relative weight, their default values are 0 and 1 respectively.
 {granularity}: In case of additive and in case of relative: can be any positive number
-{heuristic method}: pa, ps, sc  
+{heuristic method}: pa, ps, sc
 {type of weight}: a for additive and r for relative
 {with or without elicitation cost}: 0 for without elicitation cost and 1 for with elicitation cost
 {path to read the output problem}: absolute path to the xml input file and generated outputs
-{name of the output file}: name of the output 
+{name of the output file}: name of the output
 
 Example: python GenerateTree.py Rnd5-7-1.xml 1 0 ps r 0 ./ 1
 
 
 Output numbers shown after running the above command are as follows: lower bound on the cost tables , upper bound on the cost tables , the total solution costs (updated final upper bound), actual constraint cost before adding elicitation costs to it , accumulated elicited cost , number of elicited constraints , total number of unknown costs in the problem, runtime of solving the problem , elicitation costs of the optimal solution it self which is not cumulative
-
